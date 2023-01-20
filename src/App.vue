78a2d156b4f89e1ff65186c304da9d57bca070b4
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-20 14:29:28 +0100

    created and imported new components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,74 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
 export default {
+  data() {
+    links = [
+      {
+        text: 'Characters',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Comics',
+        url: '#',
+        current: true,
+      },
+      {
+        text: 'Movies',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'TV',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Games',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Collectibles',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Videos',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Fans',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'News',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Shop',
+        url: '#',
+        current: false,
+      },
+    ];
+
+  },
+
+  components: { AppHeader, AppMain, AppFooter }
 
 }
 </script>
 
 <template>
+  <app-header></app-header>
+  <app-main></app-main>
+  <app-footer></app-footer>
 
 </template>
 
