3e4bc2904cf78934e79ebb408dd0828997f7eb21
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-20 18:37:02 +0100

    alignment links lists

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,43 +9,63 @@ export default {
     <footer>
         <div class="footer-top">
             <div class="container">
-                <p>FOOTER TOP</p>
-            </div>
-        </div>
-        <div class="footer-bottom">
-            <div class="container">
                 <div class="link-lists">
                     <div class="left-lists">
                         <nav class="top-list">
                             <h2 class="title-list">DC COMICS</h2>
                             <ul>
-                                <li><a href="#">acqua</a></li>
-
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
                             </ul>
                         </nav>
                         <nav class="bottom-list">
                             <h2 class="title-list">SHOP</h2>
                             <ul>
-                                <li><a href="#">acqua</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Movies</a></li>
                             </ul>
                         </nav>
                     </div>
                     <nav class="center-list">
                         <h2 class="title-list">DC</h2>
                         <ul>
-                            <li><a href="#">acqua</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
                         </ul>
                     </nav>
                     <nav class="right-list">
                         <h2 class="title-list">SITES</h2>
                         <ul>
-                            <li><a href="#">acqua</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Movies</a></li>
                         </ul>
                     </nav>
                 </div>
                 <figure>
                     <img src="" alt="">
                 </figure>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+
             </div>
         </div>
     </footer>
@@ -63,11 +83,29 @@ export default {
 
 .link-lists {
     display: flex;
-    align-items: center;
+    padding: 2rem 0;
+}
+
+.top-list {
+    margin-bottom: 1.5rem;
+}
+
+.center-list {
+    margin: 0 2rem;
 }
 
 .footer-bottom {
     background-color: cornflowerblue;
     min-height: 350px;
+}
+
+/* Utils */
+h2 {
+    color: white;
+    margin-bottom: 1rem;
+}
+
+.link-lists a {
+    color: rgba(255, 255, 255, 0.3);
 }
 </style>
