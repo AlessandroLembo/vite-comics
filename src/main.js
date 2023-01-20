6ccf7f4974b60ed8b1508ac1eeaa8e0db124f1ff
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-20 15:33:35 +0100

    imported fonts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import "@fontsource/open-sans"
+import "@fontsource/open-sans-condensed"
 
 createApp(App).mount('#app')
