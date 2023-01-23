68a175ceaa0edcded905c4577c756da75cd2cb13
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-23 17:11:38 +0100

    changed array's name

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,13 @@
 <script>
 import AppMainContent from './AppMainContent.vue';
-import AppCard from './AppCard.vue';
+import AppCard from './articles/AppCard.vue';
 
 export default {
     name: 'AppMain',
     components: { AppMainContent, AppCard },
     data() {
         return {
-            cards: [
+            articles: [
                 {
                     thumb:
                         'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX',
---
+++ src/components/articles/AppCard.vue
@@ -0,0 +1,64 @@
+<script>
+
+export default {
+    name: 'AppCard',
+}
+</script>
+
+<template>
+    <section id="content-top">
+        <div class="container main-top-content">
+            <div v-for="article in articles" :key="article.series" class="col">
+                <div class="card">
+                    <img :src="article.thumb" :alt="article.type">
+                    <h3> {{ article.series }} </h3>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/mixins' as *;
+@use '../assets/scss/partials/variables' as *;
+
+/* SECTION TOP CONTENT */
+#content-top {
+    background-color: rgba(0, 0, 0, 0.8);
+    min-height: 130px;
+}
+
+.main-top-content {
+    min-height: $h100;
+    @include vertical_algnm;
+    flex-wrap: wrap;
+    padding-bottom: 1.5rem;
+}
+
+.col {
+    flex-basis: calc(100% / 6);
+    height: 240px;
+    margin: 2rem auto 0.5rem;
+    padding: 0 1rem;
+}
+
+.card {
+    // border: 1px dashed blue;
+    height: $h100;
+
+    img {
+        height: 80%;
+        width: 100%;
+        object-fit: cover;
+        object-position: 100% 0%;
+        display: block;
+    }
+
+    h3 {
+        padding-top: 10px;
+        color: white;
+        font-size: 15px;
+    }
+}
+</style>
